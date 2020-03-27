0601c2c8b1097b798f64e04bc79ac877943db8ee
Mateusz Blyszcz <mat@superformula.com>
2020-03-27 16:17:15 +0100

    Update description

--- packages/TextButton/src/TextButton.tsx
+++ packages/TextButton/src/TextButton.tsx
@@ -5,7 +5,7 @@ import { Text } from "@mat-sf/my-awesome-lib-text";
 export const TextButton = () => {
   return (
     <div>
-      <h2>I'm composed of two components!</h2>
+      <h2>I'm composed of two components! Hello Julian!</h2>
       <Text>It's text</Text>
       <Button>It's button</Button>
     </div>
