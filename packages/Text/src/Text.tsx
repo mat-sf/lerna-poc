2c7ff1c99a8a58653d35b4e0a78473b4ec8e2f0d
Mateusz Blyszcz <mat@superformula.com>
2020-03-27 16:19:00 +0100

    Bigger text size

--- packages/Text/src/Text.tsx
+++ packages/Text/src/Text.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 const StyledParagraph = styled.p`
   color: ${props => props?.theme?.main || "green"};
-  font-size: 28px;
+  font-size: 30px;
 `;
 
 export const Text = ({ children }: TextProps) => {
